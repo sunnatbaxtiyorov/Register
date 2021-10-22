94631a26573f5390184699d686caf4b0c2a6ee9d
Sunnat <87523986+sunnatbaxtiyorov@users.noreply.github.com>
2021-10-22 15:22:27 +0500

    modified

--- script.js
+++ script.js
@@ -23,8 +23,51 @@ function deletePhoto() {
 
 
 
-function addName(){
-    const name = document.getElementById("name").value;
-    localStorage.setItem("name",name);
-}
+// function addName(){
+//     const name = document.getElementById("name").value;
+//     localStorage.setItem("name",name);
+// }
 
+// function addSurname(){
+//     const surname = document.getElementById("surname").value;
+//     localStorage.setItem("surname" , surname);
+// }
+
+// function addNumber(){
+//     const number = document.getElementById("number").value;
+//     localStorage.setItem("number" , number);
+// }
+
+// function addJob(){
+//     const job = document.getElementById('job').value;
+//     localStorage.setItem("job" , job);
+// }
+
+// function addGender(){
+//     const gender = document.getElementById("gender").value;
+//     localStorage.setItem("gender" , gender);
+// }
+
+const send = document.querySelector(".send");
+
+// send.addEventListener("click",addSurname);
+// send.addEventListener("click",addName );
+// send.addEventListener("click",addNumber);
+// send.addEventListener("click",addJob);
+// send.addEventListener("click",addGender);
+
+send.addEventListener("click",addInfo);
+
+
+
+function addInfo(){
+    const info = {
+        name: document.getElementById('name').value,
+        surname:document.getElementById("surname").value,
+        phone : document.getElementById("number").value,
+        job : document.getElementById("job").value,
+        gender : document.getElementById("gender").value
+    }
+
+    localStorage.setItem("info",JSON.stringify(info));
+}
